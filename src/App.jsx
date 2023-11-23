12605febe7c7dd82fe00f7aee0107e874e1647cc
YvetteNyibuka <izanyibukayvette@gmail.com>
2023-11-23 23:37:37 +0200

    ft-home-page

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Dashboard from './watch/Dashboard'
-import Sidebar from './watch/Sidebar'
-// import './App.css'
-import Home from './watch/Home'
-import Video from './watch/Video'
-import Balance from './watch/Balance'
-import Landingpage from './landingpage/Landingpage'
-import Signupform from './landingpage/Signupform'
-import Login from './landingpage/Login'
-import Settings from './watch/Setting'
-
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Dashboard from "./watch/Dashboard";
+import Sidebar from "./watch/Sidebar";
+import Video from "./watch/Video";
+import Balance from "./watch/Balance";
+import Landingpage from "./landingpage/Landingpage";
+import Signupform from "./landingpage/Signupform";
+import Login from "./landingpage/Login";
+import Settings from "./watch/Setting";
+import Home from "./landingpage/Home";
 function App() {
-  
-
   return (
-    
     <BrowserRouter>
-   <Routes>
-<Route path="/" element={<Landingpage/>}>
-  <Route path='signup' element={<Signupform/>} />
-  <Route index element={<Login/>} />
-</Route>
+      <Routes>
+        <Route path="/" element={<Landingpage />}>
+          <Route index element={<Home/>} />
+          <Route path="signup" element={<Signupform />} />
+          <Route path="login" element={<Login />} />
+        </Route>
 
-
-     <Route path="/dasboard" element={<Dashboard />}>
-       <Route index element={<Home/>} /> 
-       <Route index path='home' element={<Home/>} /> 
-       <Route path="video" element={<Video/>} />
-       <Route path="balance" element={<Balance />} />
-       <Route path="setting" element={<Settings/>} />
-     </Route>
-     </Routes>
+        <Route path="/dasboard" element={<Dashboard />}>
+          <Route index element={<Home />} />
+          <Route index path="home" element={<Home />} />
+          <Route path="video" element={<Video />} />
+          <Route path="balance" element={<Balance />} />
+          <Route path="setting" element={<Settings />} />
+        </Route>
+      </Routes>
     </BrowserRouter>
-  
-    
-  )
+  );
 }
 
-export default App
-
+export default App;
---
+++ src/landingpage/Home.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import "../Styles/home.css";
+import pic from "../../public/images/homepiccc.png";
+
+const Home = () => {
+  return (
+    <div className="home-continer">
+      <div className="home-picture">
+        <img src={pic} alt="" style={{ width: "50%" }} />
+      </div>
+      <div className="home-description">
+        <h2 id="title1">Boost Your YouTube Channel, Earn Rewards!</h2>
+        <p id="desc1">
+          Welcome to ORGIN GROUP LTD, the perfect place where content creators
+          and viewers can benefit from each other. Are you a YouTuber looking to
+          boost your video views? Or maybe you enjoy watching videos and would
+          like to earn rewards while doing so? Look no further - you have come
+          to the right place!
+        </p>
+        <button className="get">Get Starterd</button>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
