00cc03635bb112a57fdbb99fb168b231201818b3
YvetteNyibuka <izanyibukayvette@gmail.com>
2023-11-24 15:27:50 +0200

    ft-homepage & signup stuffs

--- src/landingpage/Home.jsx
+++ src/landingpage/Home.jsx
@@ -1,25 +1,32 @@
 import React from "react";
 import "../Styles/home.css";
 import pic from "../../public/images/homepiccc.png";
+import VideoSection from "./VideoSection";
+import VideoCard from "./VideoCards";
 
 const Home = () => {
   return (
-    <div className="home-continer">
-      <div className="home-picture">
-        <img src={pic} alt="" style={{ width: "50%" }} />
+    <>
+      <div className="home-continer">
+        <div className="home-picture">
+          <img src={pic} alt="" style={{ width: "70%" }} />
+        </div>
+        <div className="home-description">
+          <h2 id="title1">Boost Your YouTube Channel, Earn Rewards!</h2>
+          <p id="desc1">
+            Welcome to ORGIN GROUP LTD, the perfect place where content creators
+            and viewers can benefit from each other. Are you a YouTuber looking
+            to boost your video views? Or maybe you enjoy watching videos and
+            would like to earn rewards while doing so? Look no further - you
+            have come to the right place!
+          </p>
+          <button className="get">Get Starterd</button>
+        </div>
       </div>
-      <div className="home-description">
-        <h2 id="title1">Boost Your YouTube Channel, Earn Rewards!</h2>
-        <p id="desc1">
-          Welcome to ORGIN GROUP LTD, the perfect place where content creators
-          and viewers can benefit from each other. Are you a YouTuber looking to
-          boost your video views? Or maybe you enjoy watching videos and would
-          like to earn rewards while doing so? Look no further - you have come
-          to the right place!
-        </p>
-        <button className="get">Get Starterd</button>
-      </div>
-    </div>
+
+      <VideoSection />
+      <VideoCard/>
+    </>
   );
 };
 
--- src/landingpage/Signupform.jsx
+++ src/landingpage/Signupform.jsx
@@ -1,82 +1,160 @@
-import './Signupform.css'
-import img from '../images/welcome.png'
-function Signupform()
-{
-    return(
+import React, { useState } from "react";
+import "./Signupform.css";
+import img from "../images/welcome.png";
 
-<div className='signup'>
-<div className='signup-acount'>
-  <b>
-  Create account!
-  </b>
-  <div>
-    <img src={img} className='signupimage' />
-  </div>
-</div>
+function Signupform() {
+  const [activeForm, setActiveForm] = useState("Youtuber");
+  const switchForm = (formType) => {
+    setActiveForm(formType);
+  };
+  const renderForm = () => {
+    if (activeForm === "Youtuber") {
+      return (
+        <form action="process_signup.php" method="post" className="signup-form">
+          <label for="fullname">Full Name:</label>
+          <input type="text" id="fullname" name="fullname" required />
 
+          <label for="email">Email:</label>
+          <input type="email" id="email" name="email" required />
 
-<div>
-<form action="process_signup.php" method="post" className='signup-form'>
-  
-  <label for="fullname">Full Name:</label>
-  <input type="text" id="fullname" name="fullname" required />
+          <div className="country-phone">
+            <div>
+              <label for="country">Country:</label>
+              <input type="text" id="country" name="country" required />
+            </div>
 
- 
-  <label for="email">Email:</label>
-  <input type="email" id="email" name="email" required />
+            <div>
+              <label for="phone">Phone:</label>
+              <input type="tel" id="phone" name="phone" required />
+            </div>
+          </div>
 
-  <div className='country-phone'>
-    <div>
-    <label for="country">Country:</label>
-  <input type="text" id="country" name="country" required />
-    </div>
+          <div className="gender-age">
+            <div>
+              <label for="gender">Gender:</label>
+              <select
+                id="gender"
+                name="gender"
+                required
+                className="select-gender"
+              >
+                <option value="male" style={{ color: "#191943" }}>
+                  Male
+                </option>
+                <option value="female" style={{ color: "#191943" }}>
+                  Female
+                </option>
+                <option value="other" style={{ color: "#191943" }}>
+                  Other
+                </option>
+              </select>
+            </div>
 
-  <div>
-  <label for="phone">Phone:</label>
-  <input type="tel" id="phone" name="phone" required />
-  </div>
-  
-  </div>
- 
+            <div>
+              <label for="age">Age:</label>
+              <input type="number" name="age" required />
+            </div>
+          </div>
 
-  <div className='gender-age'>
-    <div>
-    <label for="gender">Gender:</label>
-  <select id="gender" name="gender" required className='select-gender'>
-    <option value="male">Male</option>
-    <option value="female">Female</option>
-    <option value="other">Other</option>
-  </select>
-    </div>
+          <label for="payment">Mode of Payment:</label>
+          <select id="payment" name="payment" required>
+            <option value="credit_card" style={{ color: "#191943" }}>
+              Credit Card
+            </option>
+            <option value="paypal" style={{ color: "#191943" }}>
+              PayPal
+            </option>
+          </select>
 
+          <label for="text">Channel Name:</label>
+          <input type="text" id="channel" name="channel" required />
 
-  <div>
-  <label for="age">Age:</label>
-  <input type="number"  name="age" required />
-  </div>
+          <label for="text">Link to the channel:</label>
+          <input type="text" id="link" name="link" required />
 
-  </div>
-  
+          <label for="password">Password:</label>
+          <input type="password" id="password" name="password" required />
 
-  <label for="payment">Mode of Payment:</label>
-  <select id="payment" name="payment" required >
-    <option value="credit_card">Credit Card</option>
-    <option value="paypal">PayPal</option>
-  
-  </select>
+          <button type="submit" className="buttonn">
+            Signup
+          </button>
+        </form>
+      );
+    } else if (activeForm === "Client") {
+      return (
+        <form action="process_signup.php" method="post" className="signup-form">
+          <label for="fullname">Full Name:</label>
+          <input type="text" id="fullname" name="fullname" required />
+          <label for="email">Email:</label>
+          <input type="email" id="email" name="email" required />
+          <div className="country-phone">
+            <div>
+              <label for="country">Country:</label>
+              <input type="text" id="country" name="country" required />
+            </div>
 
- 
-  <label for="password">Password:</label>
-  <input type="password" id="password" name="password" required />
+            <div>
+              <label for="phone">Phone:</label>
+              <input type="tel" id="phone" name="phone" required />
+            </div>
+          </div>
+          <div className="gender-age">
+            <div>
+              <label for="gender">Gender:</label>
+              <select
+                id="gender"
+                name="gender"
+                required
+                className="select-gender"
+              >
+                <option value="male">Male</option>
+                <option value="female">Female</option>
+                <option value="other">Other</option>
+              </select>
+            </div>
 
-  
-  <button type="submit" className='buttonn'>Signup</button>
-</form>
+            <div>
+              <label for="age">Age:</label>
+              <input type="number" name="age" required />
+            </div>
+          </div>
+          <label for="payment">Mode of Payment:</label>
+          <select id="payment" name="payment" required>
+            <option value="credit_card">Credit Card</option>
+            <option value="paypal">PayPal</option>
+          </select>
+          <label for="password">Password:</label>
+          <input type="password" id="password" name="password" required />
+          <button type="submit" className="buttonn">
+            Signup
+          </button>
+        </form>
+      );
+    }
+  };
+
+  return (
+    <div className="signup">
+      <div className="signup-acount">
+        <b>Create account!</b>
+        <div>
+          <img src={img} className="signupimage" />
+        </div>
       </div>
 
-</div>
-
-      
-    )
+      <div className="auth-acc">
+        <div className="form-switch-buttons">
+          <button id="buttons" onClick={() => switchForm("Youtuber")}>
+            Youtuber
+          </button>
+          <button id="buttons" onClick={() => switchForm("Client")}>
+            Client
+          </button>
+        </div>
+        {renderForm()}
+      </div>
+    </div>
+  );
 }
-export default Signupform
+
+export default Signupform;
