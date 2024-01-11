ddb89a860b7c7380997631e5bffee500fc86eafb
kubwimanav <kubwimanavincent2020@gmail.com>
2024-01-11 18:23:39 +0200

    responsive-customer

--- src/landingpage/Contacts.jsx
+++ src/landingpage/Contacts.jsx
@@ -44,29 +44,28 @@ const Contacts = () => {
       <div className="content-cont">
         <div className="socialM">
           <h2 className="cont-title">Let's get in Touch with You!</h2>
-          <p className="loc">
-            <MdLocationPin className="soc1" />
-            Kigali KN403
-          </p>
-          <p className="loc">
-            <FaPhoneAlt className="soc1" />
-            +250787615313
-          </p>
-          <p className="loc">
-            <MdOutlineEmail className="soc1" />
-            origingroup@gmail299.com
-          </p>
-          <div className="iconss">
-            <FaSquareXTwitter className="soc" />
-            <FaInstagram className="soc" />
-            <FaTiktok className="soc" />
-          </div>
+            <p className="loc">
+              <MdLocationPin className="soc1" />
+              Kigali KN403
+            </p>
+            <p className="loc">
+              <FaPhoneAlt className="soc1" />
+              +250787615313
+            </p>
+            <p className="loc">
+              <MdOutlineEmail className="soc1" />
+              origingroup@gmail299.com
+            </p>
+            <div className="iconss">
+              <FaSquareXTwitter className="soc" />
+              <FaInstagram className="soc" />
+              <FaTiktok className="soc" />
+            </div>
         </div>
         <div className="contact-form">
           <h2>Contact Us</h2>
 
           <form method="post" onSubmit={handleSubmit} className="form-contact">
-
             <div className="form-group">
               <label htmlFor="name">Your Name</label>
               <input
